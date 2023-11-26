70c83a8db694c81c0528e1eae589b6109ee9c82a
Sowad Khan <58494210+sowad67@users.noreply.github.com>
2023-11-26 14:03:02 -0500

    fade in

--- script.js
+++ script.js
@@ -41,12 +41,12 @@ TxtType.prototype.tick = function() {
     }, delta);
 };
 
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
+var elements = document.getElementsByClassName('typewrite');
+for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-type');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
     }
 
     // INJECT CSS
@@ -55,21 +55,47 @@ TxtType.prototype.tick = function() {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 }
+
+// Add scroll event listener for arrows
+window.addEventListener("scroll", function () {
+    var arrows = document.querySelector('.arrows');
+    var scrollPosition = window.scrollY || window.pageYOffset;
+
+    // Adjust this value based on when you want the rotation to occur
+    var scrollThreshold = document.documentElement.scrollHeight - window.innerHeight - 100;
+
+    if (scrollPosition >= scrollThreshold) {
+        arrows.style.transform = 'rotate(180deg) translateY(70px)';
+    } else {
+        arrows.style.transform = 'none'; // Reset rotation
+    }
+});
+
+// Function to check if an element is in the viewport
+function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    window.addEventListener("scroll", function() {
-        var arrows = document.querySelector('.arrows');
-        var scrollPosition = window.scrollY || window.pageYOffset;
+// Function to handle the scroll event for fade-in elements
+function handleScroll() {
+    var fadeElements = document.querySelectorAll('.fade-in-left, .fade-in-right, .fade-in-top, .fade-in-bottom');
 
-        // Adjust this value based on when you want the rotation to occur
-        var scrollThreshold = document.documentElement.scrollHeight - window.innerHeight - 100;
-
-        if (scrollPosition >= scrollThreshold) {
-            arrows.style.transform = 'rotate(180deg) translateY(70px)';
-        } else {
-            arrows.style.transform = 'none'; // Reset rotation
+    fadeElements.forEach(function(element) {
+        if (isInViewport(element) && !element.classList.contains('visible')) {
+            element.classList.add('visible');
         }
     });
+}
 
-});
+// Add scroll event listener for fade-in elements
+window.addEventListener("scroll", handleScroll);
+
+// Initial check on page load for fade-in elements
+handleScroll();
+};
